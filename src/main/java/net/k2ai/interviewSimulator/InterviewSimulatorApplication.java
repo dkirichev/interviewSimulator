d9c6dd1b9017e6c8045b3e74e728d843af8ea5c5
dkirichev <dkirichev@pragma-source.com>
2026-01-04 22:08:41 +0200

    Initial commit

---
+++ src/main/java/net/k2ai/interviewSimulator/InterviewSimulatorApplication.java
@@ -0,0 +1,13 @@
+package net.k2ai.interviewSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewSimulatorApplication.class, args);
+	}
+
+}// InterviewSimulatorApplication
---
+++ src/test/java/net/k2ai/interviewSimulator/InterviewSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.k2ai.interviewSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
